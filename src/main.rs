3214bfff08f895ab077c21f2a730fab42ae374b8
Vitaly _Vi Shukela <vi0oss@gmail.com>
2020-06-24 03:23:30 +0300

    First real version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use http::method::Method;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
@@ -8,32 +10,59 @@ const POST: Method = Method::POST;
 use http::header::ACCESS_CONTROL_ALLOW_ORIGIN;
 use http::header::CONTENT_TYPE;
 
-struct MyService {}
+use dashmap::DashMap as HashMap;
+use tokio::sync::broadcast::{Sender,Receiver,channel};
+
+
+struct Loc {
+    s: Sender<String>,
+    //r: Receiver<String>,
+}
+
+struct MyService {
+    locs: HashMap<String, Loc>,
+}
 
 impl MyService {
     fn new() -> Self {
-        Self {}
+        Self {
+            locs: HashMap::new(),
+        }
     }
 
     async fn handle(&self, _req: Request<Body>) -> Result<Response<Body>, Infallible> {
         Ok(match *_req.method() {
             GET => {
+                let mut r = {
+                    let p = _req.uri().path();
+                    let l = self.locs.entry(p.to_string()).or_insert_with(||{
+                        let (s,_r) = channel(2);
+                        Loc { s }
+                    });
+                    l.s.subscribe()
+                };
                 let (mut ch, body) = Body::channel();
                 tokio::spawn(async move {
-                    let _ = ch
-                        .send_data(bytes::Bytes::from_static(b"data: 1\n\n"))
+                    while let Ok(msg) = r.recv().await {
+                        for l in msg.lines().map(|x|x.to_string()) {
+                            let _ = ch
+                            .send_data(bytes::Bytes::from_static(b"data: "))
+                            .await;
+
+                        let _ = ch
+                        .send_data(bytes::Bytes::from(l))
                         .await;
-                    tokio::time::delay_for(std::time::Duration::from_secs(1)).await;
-                    let _ = ch
-                        .send_data(bytes::Bytes::from_static(b"data: 2\n\n"))
+
+                            let _ = ch
+                            .send_data(bytes::Bytes::from_static(b"\n"))
+                            .await;
+                        }
+
+                        let _ = ch
+                        .send_data(bytes::Bytes::from_static(b"\n"))
                         .await;
-                    tokio::time::delay_for(std::time::Duration::from_secs(1)).await;
-                    let _ = ch
-                        .send_data(bytes::Bytes::from_static(b"data: 3\n\n"))
-                        .await;
-                    let _ = ch
-                        .send_data(bytes::Bytes::from_static(b""))
-                        .await;
+
+                    }
                 });
                 Response::builder()
                     .header(CONTENT_TYPE, "text/event-stream")
@@ -42,7 +71,29 @@ impl MyService {
                     .body(body)
                     .unwrap()
             }
-            POST => Response::new(Body::from("Hello World\n")),
+            POST => {
+                let p = _req.uri().path();
+                match self.locs.get(p) {
+                    None => {
+                        Response::builder().status(404).body(Body::empty()).unwrap()
+                    }
+                    Some(l) => {
+                        let s = l.s.clone();
+                        drop(l);
+                        let b = _req.into_body();
+                        tokio::spawn(async move {
+                            use bytes::Buf;
+                            if let Ok(msg) = hyper::body::aggregate(b).await {
+                                if let Ok(msg) = String::from_utf8(msg.bytes().to_vec()) {
+                                    s.send(msg);
+                                }
+                            }
+                        });
+                        Response::builder().status(204).body(Body::empty()).unwrap()
+                    }
+                }
+                
+            },
             _ => Response::builder()
                 .status(400)
                 .body(Body::from("Method should be GET or POST\n"))
